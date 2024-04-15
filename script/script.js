0f67b096a617c34bb68679537a4efb8fcdb6c44e
Snayder Rodrigues <snayderrodrigues@gmail.com>
2024-04-14 23:25:45 -0300

    fix smooth scrolling for links

--- script/script.js
+++ script/script.js
@@ -123,3 +123,15 @@ function nextImage() {
 //         faq.classList.toggle('active');
 //     });
 // });
+
+document.querySelectorAll('a[href^="#"]').forEach((el) => {
+  el.addEventListener("click", (e) => {
+    e.preventDefault();
+    const id = el.getAttribute("href")?.slice(1);
+    if (!id) return;
+    const target = document.getElementById(id);
+    if (target) {
+      target.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
