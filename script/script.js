f2f8d2d1de156ca1b5d8cc386375279dc1e02c83
Snayder Rodrigues <snayderrodrigues@gmail.com>
2024-05-03 23:39:43 -0300

    removed lenis scrolling lib

--- script/script.js
+++ script/script.js
@@ -76,66 +76,6 @@ function nextImage() {
   document.getElementById("radio-" + count).checked = true;
 }
 
-// let sections = document.querySelectorAll('section');
-// let navLinks = document.querySelectorAll('header div nav div ul li a');
-
-// window.onscroll = () => {
-//   sections.forEach(sec => {
-//     let top = window.scrollY;
-//     let offset = sec.offsetTop - 350;
-//     let height = sec.offsetHeight;
-//     let id = sec.getAttribute('id');
-
-//     if(top >= offset && top < offset + height) {
-//       navLinks.forEach(links => {
-//         links.classList.remove('active');
-//         document.querySelector('header div nav div ul li a[href*=' + id + ']').classList.add('active');
-//       });
-//     };
-//   });
-// };
-
-// const checkboxes = document.querySelectorAll('input[name="accordion"]');
-//     checkboxes.forEach((checkbox) => {
-//         checkbox.addEventListener('change', function() {
-//             checkboxes.forEach((cb) => {
-//                 if (cb !== this) {
-//                     cb.checked = false;
-//                 }
-//             });
-//         });
-//     });
-
-// const faqs = document.querySelectorAll('.faq__question');
-
-// faqs.forEach((faq) => {
-
-//     faq.addEventListener('click', () => {
-
-//         // Remove 'active' class from all elements
-//         faqs.forEach((item) => {
-//             if (item !== faq) {
-//                 item.classList.remove('active');
-//             }
-//         });
-
-//         // Toggle 'active' class for the clicked element
-//         faq.classList.toggle('active');
-//     });
-// });
-
-document.querySelectorAll('a[href^="#"]').forEach((el) => {
-  el.addEventListener("click", (e) => {
-    e.preventDefault();
-    const id = el.getAttribute("href")?.slice(1);
-    if (!id) return;
-    const target = document.getElementById(id);
-    if (target) {
-      target.scrollIntoView({ behavior: "smooth" });
-    }
-  });
-});
-
 var loader = document.getElementById("preloader");
 
 window.addEventListener("load", function() {
