1e559c3dfc8d02c6f2c9c1fd5784653000157dfd
Snayder Rodrigues <snayderrodrigues@gmail.com>
2024-06-11 23:57:06 -0300

    add store page header styling on scroll event

--- script/store-script.js
+++ script/store-script.js
@@ -7,3 +7,16 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+
+let lastScrollTop = 0;
+
+window.addEventListener("scroll", function () {
+  const header = document.querySelector(".storeHeader");
+  const currentScroll = window.scrollY;
+
+  if (currentScroll > lastScrollTop) {
+    header.classList.add("opaque");
+  } else {
+    header.classList.remove("opaque");
+  }
+});
